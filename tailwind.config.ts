b3bc4d231b183a275238cbc28400b5b41eb77eba
Jake <jakebailes76@yahoo.com>
2023-10-03 01:30:57 -0400

    navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: { display: ['Roboto', 'sans-serif'], body: ['Roboto', 'sans-serif'] },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
