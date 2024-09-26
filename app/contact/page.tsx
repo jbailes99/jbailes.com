13d6114df8a5f682c84982fc3e860f010a776795
Jake <jakebailes76@yahoo.com>
2024-09-26 04:24:43 -0400

    contact page fix mobile:

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { SocialIcon } from 'react-social-icons'
-import 'react-social-icons/github'
-import 'react-social-icons/linkedin'
-import 'react-social-icons/email'
-import 'react-social-icons/whatsapp'
 
 export default function Contact() {
   return (
-    <div className='flex-1  justify-center p-8 overflow-hidden'>
-      <div className='relative h-full isolate overflow-hidden bg-gray-800 px-8 py-24 shadow-lg rounded-2xl '>
+    <div className='flex-1 justify-center p-8 overflow-auto'>
+      <div className=' bg-gray-800 px-8 py-24 shadow-lg h-full rounded-2xl'>
         <div>
           <h2 className='mx-auto text-gray-200 max-w-2xl text-center text-3xl font-bold tracking-tight sm:text-5xl'>
             Let’s keep in touch.
@@ -64,6 +59,7 @@ export default function Contact() {
             </div>
           </div>
         </div>
+
         <div className='relative'>
           <div
             className='inset-0 mt-12 w-3/4 mx-auto flex items-center'
@@ -72,12 +68,12 @@ export default function Contact() {
             <div className='w-full border-t border-gray-400' />
           </div>
         </div>
+
         <div className='mt-12 flex flex-col items-center'>
-          {' '}
-          <h2 className='mx-auto text-gray-200 max-w-2xl text-center text-3xl font-bold tracking-tight  sm:text-5xl'>
+          <h2 className='mx-auto text-gray-200 max-w-2xl text-center text-3xl font-bold tracking-tight sm:text-5xl'>
             Say hello.
           </h2>
-          <dl className='mt-12 space-y-12  text-2xl leading-6 text-gray-900'>
+          <dl className='mt-12 space-y-12 text-2xl leading-6 text-gray-900'>
             <div className=''>
               <dt className='sr-only'></dt>
               <dd className='flex items-center'>
@@ -115,6 +111,7 @@ export default function Contact() {
             </div>
           </dl>
         </div>
+
         <div className='relative'>
           <div
             className='inset-0 mt-12 w-3/4 mx-auto flex items-center'
@@ -123,19 +120,19 @@ export default function Contact() {
             <div className='w-full border-t border-gray-400' />
           </div>
         </div>
+
         <div className='mt-12 flex flex-col items-center'>
-          <h2 className='mx-auto text-gray-200   max-w-2xl text-center text-3xl font-bold tracking-tight  sm:text-5xl'>
+          <h2 className='mx-auto text-gray-200 max-w-2xl text-center text-3xl font-bold tracking-tight sm:text-5xl'>
             Resume
           </h2>
           <div className='mt-4'>
-            <dt className='sr-only '>resume</dt>
+            <dt className='sr-only'>Resume</dt>
             <dd className='font-semibold text-gray-200 flex items-center'>
               <a
                 href='Resume.pdf'
                 download
-                className='  text-gray-300 hover:text-orange-400 hover:cursor-pointer'
+                className='text-gray-300 hover:text-orange-400 hover:cursor-pointer'
               >
-                {' '}
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   className='h-12 w-12 text-gray-100 hover:text-orange-400 mr-2'
@@ -147,146 +144,6 @@ export default function Contact() {
                 </svg>
               </a>
             </dd>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-  return (
-    <div className='flex-1  justify-center p-8'>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='mx-auto max-w-2xl space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none'>
-          <div className='mx-auto max-w-2xl sm:text-center'>
-            <div className='flex justify-center items-center'>
-              <h2 className='text-4xl mt-32 font-bold tracking-tight text-gray-900'>
-                Get in touch
-              </h2>
-              {/* <p className=' text-lg leading-8 text-gray-600'>check me out</p> */}
-              {/* <p className='mt-4 leading-7 text-gray-600'>
-                Quam nunc nunc eu sed. Sed rhoncus quis ultricies ac pellentesque.
-              </p> */}
-            </div>
-
-            <div className='text-center'>
-              <div className='rounded-2xl bg-white p-5'>
-                <h3 className='text-base font-semibold leading-7 text-gray-900'></h3>
-                <dl className='mt-3 space-y-1 text-lg leading-6 text-gray-600'>
-                  <div>
-                    <dt className='sr-only'>Email</dt>
-                    <dd>
-                      <SocialIcon
-                        url='www.github.com'
-                        href='https://github.com/jbailes99'
-                        target='_blank'
-                        style={{
-                          width: '28px',
-                          height: '28px',
-                          marginRight: '10px',
-                        }}
-                      />
-
-                      <a
-                        className='font-semibold text-indigo-600'
-                        href='https://github.com/jbailes99'
-                        target='_blank'
-                      >
-                        jbailes99
-                      </a>
-                    </dd>
-                  </div>
-                  <div className='mt-1'>
-                    {/* <dt className='sr-only'>Phone number</dt>
-                    <dd>+1 (555) 905-2345</dd> hiiiiiiii ur really coding tho*/}
-                  </div>
-                </dl>
-              </div>
-
-              <div className='rounded-2xl bg-white p-5 mt-0'>
-                <h3 className='text-base font-semibold leading-7 text-gray-900'></h3>
-                <dl className='mt-3 space-y-1 text-lg leading-6 text-gray-600'>
-                  <div>
-                    <dt className='sr-only'>Email</dt>
-                    <dd>
-                      <SocialIcon
-                        url='www.linkedin.com'
-                        href='https://www.linkedin.com/in/jbailes01/'
-                        target='_blank'
-                        style={{
-                          width: '28px',
-                          height: '28px',
-                          marginRight: '10px',
-                        }}
-                      />
-                      <a
-                        className='font-semibold text-indigo-600'
-                        href='https://www.linkedin.com/in/jbailes01/'
-                        target='_blank'
-                      >
-                        jbailes01
-                      </a>
-                    </dd>
-                  </div>
-                  <div className='mt-1'>
-                    {/* <dt className='sr-only'>Phone number</dt>
-                    <dd>+1 (555) 905-4567</dd> */}
-                  </div>
-                </dl>
-              </div>
-
-              <div className='relative'>
-                <div
-                  className='absolute inset-4 mt-12 flex items-center'
-                  aria-hidden='true'
-                >
-                  <div className='w-full border-t border-gray-300' />
-                </div>
-              </div>
-
-              <div className='rounded-2xl bg-white p-0 mt-0'>
-                <h3 className='text-3xl mt-44 font-semibold leading-7 text-gray-900'>
-                  Say hello
-                </h3>
-                <dl className='mt-6 space-y-8 text-xl leading-6 text-gray-900'>
-                  <div>
-                    <dt className='sr-only'></dt>
-                    <dd>
-                      <SocialIcon
-                        url='www.email.com'
-                        href='https://github.com/jbailes99'
-                        target='_blank'
-                        style={{
-                          width: '30px',
-                          height: '30px',
-                          marginRight: '10px',
-                        }}
-                      />
-                      <a
-                        className='font-semibold text-indigo-600'
-                        href='mailto:contact@jbailes.com'
-                      >
-                        contact@jbailes.com
-                      </a>
-                    </dd>
-                  </div>
-                  <div className='mt-1'>
-                    <dt className='sr-only'>Phone number</dt>
-                    <dd className=''>
-                      <SocialIcon
-                        url='www.whatsapp.com'
-                        href='https://github.com/jbailes99'
-                        target='_blank'
-                        style={{
-                          width: '30px',
-                          height: '30px',
-                          marginRight: '10px',
-                        }}
-                      />{' '}
-                      +1 (781) 513-1582
-                    </dd>
-                  </div>
-                </dl>
-              </div>
-            </div>
           </div>
         </div>
       </div>
